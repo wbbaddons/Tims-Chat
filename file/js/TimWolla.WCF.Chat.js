8cd5aaed421d231450eb62c3c4deb80169851e49
Tim Düsterhus <timwolla@arcor.de>
2011-11-26 15:17:17 +0100

    Adding basic file-structure

---
+++ file/js/TimWolla.WCF.Chat.js
@@ -0,0 +1,15 @@
+/**
+ * TimWolla.WCF.Chat
+ * 
+ * @author	Tim Düsterhus
+ * @copyright	2010-2011 Tim Düsterhus
+ * @license	Creative Commons Attribution-NonCommercial-ShareAlike <http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode>
+ * @package	timwolla.wcf.chat
+ */
+if (typeof TimWolla == undefined) var TimWolla = {};
+if (typeof TimWolla.WCF == undefined) var TimWolla.WCF = {};
+	
+var TimWolla.WCF.Chat = {};
+(function ($, document) {
+	
+})(jQuery, document);
---
+++ file/lib/page/ChatPage.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace wcf\page;
+
+/**
+ * Shows the chat-interface
+ *
+ * @author 	Tim Düsterhus
+ * @copyright	2010-2011 Tim Düsterhus
+ * @license	Creative Commons Attribution-NonCommercial-ShareAlike <http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode>
+ * @package	timwolla.wcf.chat
+ * @subpackage	page
+ */
+class ChatPage extends AbstractPage {
+	
+}
