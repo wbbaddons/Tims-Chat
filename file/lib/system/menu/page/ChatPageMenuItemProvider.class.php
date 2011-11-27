5c72cd52be6e5c0a62e0cd52d3cb9536247a916f
Tim Düsterhus <timwolla@arcor.de>
2011-11-27 12:46:10 +0100

    Adding PageMenuItem for chat

---
+++ file/lib/system/menu/page/ChatPageMenuItemProvider.class.php
@@ -0,0 +1,28 @@
+<?php
+namespace wcf\system\menu\page;
+
+/**
+ * PageMenuItemProvider for chat.
+ *
+ * @author 	Tim Düsterhus
+ * @copyright	2010-2011 Tim Düsterhus
+ * @license	Creative Commons Attribution-NonCommercial-ShareAlike <http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode>
+ * @package	timwolla.wcf.chat
+ * @subpackage	system.menu.page
+ */
+class ChatPageMenuItemProvider extends DefaultPageMenuItemProvider {
+	/**
+	 * Hides the button when there is no valid room
+	 *
+	 * @see wcf\system\menu\page\PageMenuItemProvider::isVisible()
+	 */
+	public function isVisible() {
+		try {
+			\wcf\data\chat\room\ChatRoom::getCache()->seek(0);
+			return true;
+		}
+		catch (\OutOfBoundsException $e) {
+			return false;
+		}
+	}
+}
