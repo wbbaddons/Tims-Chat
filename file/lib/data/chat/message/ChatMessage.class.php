768c4f0066e398f186e7364dad5d2881853b19ff
Tim Düsterhus <timwolla@arcor.de>
2011-12-05 21:14:14 +0100

    Add <time>-Tag

--- file/lib/data/chat/message/ChatMessage.class.php
+++ file/lib/data/chat/message/ChatMessage.class.php
@@ -87,7 +87,7 @@ class ChatMessage extends \wcf\data\DatabaseObject {
 		return \wcf\util\JSON::encode(array(
 			'formattedUsername' => $this->getFormattedUsername(),
 			'formattedMessage' => $this->getFormattedMessage(),
-			'formattedTime' => \wcf\util\DateUtil::format(\wcf\util\DateUtil::getDateTimeByTimestamp($this->time), 'H:i:s'),
+			'formattedTime' => '<time>'.\wcf\util\DateUtil::format(\wcf\util\DateUtil::getDateTimeByTimestamp($this->time), 'H:i:s').'</time>',
 			'sender' => $this->sender,
 			'username' => $this->getUsername(),
 			'time' => $this->time,
