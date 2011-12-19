d275f86498ea74e3f6cce413968d8aa5c39984c7
Tim Düsterhus <timwolla@arcor.de>
2011-12-19 15:46:40 +0100

    Adding JSDoc

--- file/js/TimWolla.WCF.Chat.cs
+++ file/js/TimWolla.WCF.Chat.cs
@@ -1,3 +1,12 @@
+###
+TimWolla.WCF.Chat
+  	
+@author  Tim Düsterhus
+@copyright  2010-2011 Tim Düsterhus
+@license  Creative Commons Attribution-NonCommercial-ShareAlike <http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode>
+@package  timwolla.wcf.chat
+###
+
 TimWolla ?= {}
 TimWolla.WCF ?= {}
 
