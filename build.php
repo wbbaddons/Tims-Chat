4c3a1014551d39f4eabc71f36b29e22a1d562c29
Tim Düsterhus <timwolla@arcor.de>
2011-12-26 15:49:33 +0100

    Testing Travis-CI

---
+++ build.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+@unlink('file.tar');
+@unlink('template.tar');
+@unlink('timwolla.wcf.chat.tar');
+#exec('coffee -c file/js/*.coffee');
+chdir('file');
+exec('tar cvf ../file.tar * --exclude=*.coffee');
+chdir('..');
+chdir('template');
+exec('tar cvf ../template.tar *');
+chdir('..');
+exec('tar cvf timwolla.wcf.chat.tar * --exclude=file --exclude=template --exclude=build.php');
+@unlink('file.tar');
+@unlink('template.tar');
+exec('rm file/js/*.js');
