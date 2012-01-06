91456cafe144ff14fafd4768da87bdd8aefdb3be
Tim Düsterhus <timwolla@arcor.de>
2012-01-06 16:15:40 +0100

    Improving build.php

--- build.php
+++ build.php
@@ -1,16 +1,61 @@
 #!/usr/bin/env php
 <?php
-@unlink('file.tar');
-@unlink('template.tar');
-@unlink('timwolla.wcf.chat.tar');
-exec('coffee -cb file/js/*.coffee');
-chdir('file');
-exec('tar cvf ../file.tar * --exclude=*.coffee');
-chdir('..');
-chdir('template');
-exec('tar cvf ../template.tar *');
-chdir('..');
-exec('tar cvf timwolla.wcf.chat.tar * --exclude=file --exclude=template --exclude=build.php');
-@unlink('file.tar');
-@unlink('template.tar');
-exec('rm file/js/*.js');
+echo <<<EOT
+Cleaning up
+-----------
+
+EOT;
+	if (file_exists('file.tar')) unlink('file.tar');
+	if (file_exists('template.tar')) unlink('template.tar');
+	foreach (glob('file/js/*.js') as $jsFile) unlink($jsFile);
+	foreach (glob('file/style/*.css') as $cssFile) unlink($cssFile);
+	if (file_exists('timwolla.wcf.chat.tar')) unlink('timwolla.wcf.chat.tar');
+echo <<<EOT
+
+Building JavaScript
+-------------------
+
+EOT;
+foreach (glob('file/js/*.coffee') as $coffeeFile) {
+	echo $coffeeFile."\n";
+	passthru('coffee -cb '.escapeshellarg($coffeeFile));
+}
+echo <<<EOT
+
+Building CSS
+------------
+
+EOT;
+foreach (glob('file/style/*.scss') as $sassFile) {
+	echo $sassFile."\n";
+	passthru('scss '.escapeshellarg($sassFile).' '.escapeshellarg(substr($sassFile, 0, -4).'css'));
+}
+echo <<<EOT
+
+Building file.tar
+-----------------
+
+EOT;
+	chdir('file');
+	passthru('tar cvf ../file.tar * --exclude=*.coffee');
+echo <<<EOT
+
+Building template.tar
+---------------------
+
+EOT;
+	chdir('../template');
+	passthru('tar cvf ../template.tar *');
+echo <<<EOT
+
+Building timwolla.wcf.chat.tar
+------------------------------
+
+EOT;
+	chdir('..');
+	passthru('tar cvf timwolla.wcf.chat.tar * --exclude=file --exclude=template --exclude=build.php');
+
+if (file_exists('file.tar')) unlink('file.tar');
+if (file_exists('template.tar')) unlink('template.tar');
+foreach (glob('file/js/*.js') as $jsFile) unlink($jsFile);
+foreach (glob('file/style/*.css') as $cssFile) unlink($cssFile);
