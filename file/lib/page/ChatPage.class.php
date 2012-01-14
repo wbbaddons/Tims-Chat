36592b0d9e17e2263a6bca1d4d76997b408953a6
Tim Düsterhus <timwolla@arcor.de>
2012-01-14 12:53:18 +0100

    Moving lastSeen a bit around to see your join when inline-room-changing

--- file/lib/page/ChatPage.class.php
+++ file/lib/page/ChatPage.class.php
@@ -66,6 +66,8 @@ class ChatPage extends AbstractPage {
 		$this->readRoom();		
 		$this->userData['color'] = \wcf\util\ChatUtil::readUserData('color');
 		\wcf\util\ChatUtil::writeUserData(array('roomID' => $this->room->roomID));
+		$this->newestMessages = chat\message\ChatMessageList::getNewestMessages($this->room, CHAT_LASTMESSAGES);
+		\wcf\util\ChatUtil::writeUserData(array('lastSeen' => end($this->newestMessages)->messageID));
 		
 		if (CHAT_DISPLAY_JOIN_LEAVE) {
 			$messageAction = new chat\message\ChatMessageAction(array(), 'create', array(
@@ -82,14 +84,10 @@ class ChatPage extends AbstractPage {
 			));
 			$messageAction->executeAction();
 			$return = $messageAction->getReturnValues();
-
-			\wcf\util\ChatUtil::writeUserData(array('lastSeen' => $return['returnValues'] -> messageID));
 		}
 		
 		$this->readDefaultSmileys();
 		$this->readChatVersion();
-		
-		$this->newestMessages = chat\message\ChatMessageList::getNewestMessages($this->room, CHAT_LASTMESSAGES);
 	}
 	
 	/**
