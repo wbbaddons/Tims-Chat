961dbcc1f983c6f45d9bfc9f41789331a1e677ad
Tim Düsterhus <timwolla@arcor.de>
2012-05-19 23:03:35 +0200

    Adding Restore-Command

---
+++ file/lib/system/chat/command/commands/Restore.class.php
@@ -0,0 +1,59 @@
+<?php
+namespace wcf\system\chat\command\commands;
+use \wcf\data\user\User;
+use \wcf\util\ChatUtil;
+
+/**
+ * Resets the color of a user
+ *
+ * @author 	Tim Düsterhus
+ * @copyright	2010-2012 Tim Düsterhus
+ * @license	Creative Commons Attribution-NonCommercial-ShareAlike <http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode>
+ * @package	timwolla.wcf.chat
+ * @subpackage	system.chat.command.commands
+ */
+class Restore extends \wcf\system\chat\command\AbstractRestrictedCommand {
+	public $enableSmilies = \wcf\system\chat\command\ICommand::SMILEY_OFF;
+	public $enableHTML = 1;
+	public $user = null;
+	public $link = '';
+	
+	public function __construct(\wcf\system\chat\command\CommandHandler $commandHandler) {
+		parent::__construct($commandHandler);
+		
+		$this->user = User::getUserByUsername(rtrim($commandHandler->getParameters(), ','));
+		if (!$this->user->userID) throw new \wcf\system\chat\command\UserNotFoundException(rtrim($commandHandler->getParameters(), ','));
+		
+		// Username + link to profile
+		$color = array(1 => ChatUtil::getRandomNumber(), 2 => ChatUtil::getRandomNumber() * 0xFFFF);
+		ChatUtil::writeUserData(array('color' => $color), $this->user);
+		
+		$profile = \wcf\system\request\LinkHandler::getInstance()->getLink('User', array(
+			'object' => $this->user
+		));
+		$this->link = '<a href="'.$profile.'">'.ChatUtil::gradient($this->user->username, $color[1], $color[2]).'</a>';
+		
+		$this->didInit();
+	}
+	
+	/**
+	 * @see	\wcf\system\chat\command\ICommand::getType()
+	 */
+	public function getType() {
+		return \wcf\data\chat\message\ChatMessage::TYPE_INFORMATION;
+	}
+	
+	/**
+	 * @see	\wcf\system\chat\command\ICommand::getMessage()
+	 */
+	public function getMessage() {
+		return 'restored '.$this->link;
+	}
+	
+	/**
+	 * @see	\wcf\system\chat\command\ICommand::getReceiver()
+	 */
+	public function getReceiver() {
+		return \wcf\system\WCF::getUser()->userID;
+	}
+}
