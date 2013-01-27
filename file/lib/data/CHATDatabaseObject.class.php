6359d0382e8258a9f7f48f92d0ed7bf4b32bafa1
max-m <maximilian.mader@gmx.de>
2013-01-27 18:14:27 +0100

    Added missing space Getting commits like a pro ... lol

--- file/lib/data/CHATDatabaseObject.class.php
+++ file/lib/data/CHATDatabaseObject.class.php
@@ -12,7 +12,7 @@ namespace chat\data;
  */
 abstract class CHATDatabaseObject extends \wcf\data\DatabaseObject {
 	/**
-	 *@see \wcf\data\DatabaseObject::getDatabaseTableName()
+	 * @see \wcf\data\DatabaseObject::getDatabaseTableName()
 	 */
 	public static function getDatabaseTableName() {
 		return 'chat'.WCF_N.'_'.static::$databaseTableName;
