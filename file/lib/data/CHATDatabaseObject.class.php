e2f355a48c7acf7b1f8fd57c51da889839b415ac
max-m <maximilian.mader@gmx.de>
2013-01-27 18:07:24 +0100

    Added missing period and missing new line

--- file/lib/data/CHATDatabaseObject.class.php
+++ file/lib/data/CHATDatabaseObject.class.php
@@ -2,7 +2,8 @@
 namespace chat\data;
 
 /**
- * Basic implementation that sets proper table name
+ * Basic implementation that sets proper table name.
+ *
  * @author	Tim Düsterhus
  * @copyright	2010-2013 Tim Düsterhus
  * @license	Creative Commons Attribution-NonCommercial-ShareAlike <http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode>
