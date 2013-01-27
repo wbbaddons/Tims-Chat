c448be28cc77102f6c76a7d93cf3b14bffc8d08c
max-m <maximilian.mader@gmx.de>
2013-01-27 17:24:00 +0100

    Fixed indentation, added class comment

--- file/lib/data/CHATDatabaseObject.class.php
+++ file/lib/data/CHATDatabaseObject.class.php
@@ -1,11 +1,19 @@
 <?php
 namespace chat\data;
 
+/**
+ * Basic implementation that sets proper table name
+ * @author	Tim Düsterhus
+ * @copyright	2010-2013 Tim Düsterhus
+ * @license	Creative Commons Attribution-NonCommercial-ShareAlike <http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode>
+ * @package	be.bastelstu.chat
+ * @subpackage	data
+ */
 abstract class CHATDatabaseObject extends \wcf\data\DatabaseObject {
 	/**
-	 * @see \wcf\data\DatabaseObject::getDatabaseTableName()
-	 */
+	* @see \wcf\data\DatabaseObject::getDatabaseTableName()
+	*/
 	public static function getDatabaseTableName() {
-	    return 'chat'.WCF_N.'_'.static::$databaseTableName;
+		return 'chat'.WCF_N.'_'.static::$databaseTableName;
 	}
 }
