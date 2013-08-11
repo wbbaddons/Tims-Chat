4bc538f367d5d148cba0c0f49fc3215a29f7d2ec
Tim Düsterhus <timwolla@arcor.de>
2013-08-11 17:30:34 +0200

    Extend HourlyCleanUpCronjob instead of an own cronjob

---
+++ file/lib/system/event/listener/HourlyCleanUpCronjobExecuteChatCleanUpListener.class.php
@@ -0,0 +1,28 @@
+<?php
+namespace chat\system\event\listener;
+use \chat\data;
+
+/**
+ * Vaporizes unneeded data.
+ *
+ * @author 	Tim Düsterhus
+ * @copyright	2010-2013 Tim Düsterhus
+ * @license	Creative Commons Attribution-NonCommercial-ShareAlike <http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode>
+ * @package	be.bastelstu.chat
+ * @subpackage	system.event.listener
+ */
+class HourlyCleanUpCronjobExecuteChatCleanUpListener implements IEventListener {
+	/**
+	 * @see	\wcf\system\event\IEventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+		$messageAction = new data\message\MessageAction(array(), 'prune');
+		$messageAction->executeAction();
+		$roomAction = new data\room\RoomAction(array(), 'prune');
+		$roomAction->executeAction();
+		
+		// kill dead users
+		$roomAction = new data\room\RoomAction(array(), 'removeDeadUsers');
+		$roomAction->executeAction();
+	}
+}
