c745c6f6632cf991d546ec7e2713c8107474e630
Tim Düsterhus <tim@bastelstu.be>
2014-08-20 00:42:43 +0200

    Add update_3.0.x_3.1.0.sql

---
+++ update_3.0.x_3.1.0.sql
@@ -0,0 +1,7 @@
+ALTER TABLE wcf1_user CHANGE chatColor1 chatColor1 INT(10) DEFAULT NULL;
+ALTER TABLE wcf1_user CHANGE chatColor2 chatColor2 INT(10) DEFAULT NULL;
+
+ALTER TABLE chat1_message CHANGE color1 color1 INT(10) DEFAULT NULL;
+ALTER TABLE chat1_message CHANGE color2 color2 INT(10) DEFAULT NULL;
+
+UPDATE wcf1_user SET chatColor1 = NULL, chatColor2 = NULL WHERE chatColor1 = 0 AND chatColor2 = 0;
