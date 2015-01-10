9ca329571f16fbf753b4f707b4e9720d6acc4e96
Tim Düsterhus <tim@bastelstu.be>
2015-01-10 15:48:20 +0100

    Add maxUsers column during update

--- update_3.0.x_3.1.0.sql
+++ update_3.0.x_3.1.0.sql
@@ -1,6 +1,8 @@
 ALTER TABLE wcf1_user CHANGE chatColor1 chatColor1 INT(10) DEFAULT NULL;
 ALTER TABLE wcf1_user CHANGE chatColor2 chatColor2 INT(10) DEFAULT NULL;
 
+ALTER TABLE chat1_room ADD COLUMN maxUsers INT(10) NOT NULL DEFAULT 0;
+
 ALTER TABLE chat1_message CHANGE color1 color1 INT(10) DEFAULT NULL;
 ALTER TABLE chat1_message CHANGE color2 color2 INT(10) DEFAULT NULL;
 
