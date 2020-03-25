5687d6fabbdac385ff75895183f648a915894935
Tim Düsterhus <tim@bastelstu.be>
2020-03-25 23:03:38 +0100

    Bump version

--- files/style/be.bastelstu.chat.scss
+++ files/style/be.bastelstu.chat.scss
@@ -4,7 +4,7 @@
  * Use of this software is governed by the Business Source License
  * included in the LICENSE file.
  *
- * Change Date: 2022-11-27
+ * Change Date: 2024-03-25
  *
  * On the date above, in accordance with the Business Source
  * License, use of this software will be governed by version 2
