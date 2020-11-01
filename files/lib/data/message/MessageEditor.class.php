e9739cd8068c9847cf94fab70d2f902b319327a7
Tim Düsterhus <tim@bastelstu.be>
2020-11-01 13:21:28 +0100

    Delete attachments when deleting messages

--- files/lib/data/message/MessageEditor.class.php
+++ files/lib/data/message/MessageEditor.class.php
@@ -1,11 +1,11 @@
 <?php
 /*
- * Copyright (c) 2010-2018 Tim Düsterhus.
+ * Copyright (c) 2010-2020 Tim Düsterhus.
  *
  * Use of this software is governed by the Business Source License
  * included in the LICENSE file.
  *
- * Change Date: 2024-10-20
+ * Change Date: 2024-11-01
  *
  * On the date above, in accordance with the Business Source
  * License, use of this software will be governed by version 2
@@ -13,6 +13,9 @@
  */
 
 namespace chat\data\message;
+
+use \wcf\system\attachment\AttachmentHandler;
+use \wcf\system\WCF;
 
 /**
  * Represents a chat message editor.
@@ -22,4 +25,20 @@ class MessageEditor extends \wcf\data\DatabaseObjectEditor {
 	 * @inheritDoc
 	 */
 	protected static $baseClass = Message::class;
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function deleteAll(array $messageIDs = []) {
+		WCF::getDB()->beginTransaction();
+
+		$result = parent::deleteAll($messageIDs);
+		if (!empty($messageIDs)) {
+			AttachmentHandler::removeAttachments('be.bastelstu.chat.message', $messageIDs);
+		}
+
+		WCF::getDB()->commitTransaction();
+
+		return $result;
+	}
 }
