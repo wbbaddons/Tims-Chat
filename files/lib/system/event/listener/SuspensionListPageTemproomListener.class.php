e661d462a3c6de090de9e082e627a6a3e62bb4c7
Tim Düsterhus <tim@bastelstu.be>
2021-03-05 17:19:27 +0100

    Bump version

--- files/lib/system/event/listener/SuspensionListPageTemproomListener.class.php
+++ files/lib/system/event/listener/SuspensionListPageTemproomListener.class.php
@@ -5,7 +5,7 @@
  * Use of this software is governed by the Business Source License
  * included in the LICENSE file.
  *
- * Change Date: 2025-02-04
+ * Change Date: 2025-03-05
  *
  * On the date above, in accordance with the Business Source
  * License, use of this software will be governed by version 3
