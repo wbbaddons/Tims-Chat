9c3f0db19654b80450755f9e85c7078b2151bc50
Tim Düsterhus <tim@bastelstu.be>
2022-03-04 21:21:52 +0100

    Bump version

--- files_wcf/js/Bastelstu.be/Chat/Command/_Unsuspension.js
+++ files_wcf/js/Bastelstu.be/Chat/Command/_Unsuspension.js
@@ -4,7 +4,7 @@
  * Use of this software is governed by the Business Source License
  * included in the LICENSE file.
  *
- * Change Date: 2025-03-05
+ * Change Date: 2026-03-04
  *
  * On the date above, in accordance with the Business Source
  * License, use of this software will be governed by version 3
