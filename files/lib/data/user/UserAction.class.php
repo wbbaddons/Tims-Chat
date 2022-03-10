wbbaddons/Tims-Chat

commit 527a04db5861d86078b68683e071919b7891c1c1

Avoid the use of `empty()`

files/lib/data/user/UserAction.class.php
@@ -88,7 +88,7 @@ class UserAction extends AbstractDatabaseObjectAction
     public function clearDeadSessions()
     {
         $sessions = User::getDeadSessions();
-        if (empty($sessions)) {
+        if ($sessions !== []) {
             return;
         }
         $userIDs = \array_map(static function ($item) {

files/lib/acp/form/CommandTriggerAddForm.class.php
@@ -101,7 +101,7 @@ class CommandTriggerAddForm extends AbstractForm
     {
         parent::validate();
 
-        if (empty($this->commandTrigger)) {
+        if ($this->commandTrigger === '') {
             throw new UserInputException('commandTrigger', 'empty');
         }
 
@@ -119,7 +119,7 @@ class CommandTriggerAddForm extends AbstractForm
             throw new UserInputException('commandTrigger', 'duplicate');
         }
 
-        if (empty($this->className)) {
+        if ($this->className === '') {
             throw new UserInputException('className', 'empty');
         }
 

files/lib/acp/form/RoomAddForm.class.php
@@ -225,7 +225,7 @@ class RoomAddForm extends AbstractForm
             }
         }
 
-        if (!empty($data)) {
+        if ($data !== []) {
             (new RoomEditor($room))->update($data);
         }
     }

files/lib/acp/page/SuspensionListPage.class.php
@@ -149,7 +149,7 @@ class SuspensionListPage extends SortablePage
         if (isset($_POST['searchUsername'])) {
             $this->searchUsername = StringUtil::trim($_POST['searchUsername']);
 
-            if (!empty($this->searchUsername)) {
+            if ($this->searchUsername !== '') {
                 $this->userID = User::getUserByUsername($this->searchUsername)->userID;
             }
         } elseif ($this->userID !== null) {
@@ -159,7 +159,7 @@ class SuspensionListPage extends SortablePage
         if (isset($_POST['searchJudge'])) {
             $this->searchJudge = StringUtil::trim($_POST['searchJudge']);
 
-            if (!empty($this->searchJudge)) {
+            if ($this->searchJudge !== '') {
                 $this->judgeID = User::getUserByUsername($this->searchJudge)->userID;
             }
         } elseif ($this->judgeID !== null) {

files/lib/data/message/MessageAction.class.php
@@ -333,7 +333,7 @@ class MessageAction extends AbstractDatabaseObjectAction
             return $message->hasEmbeddedObjects;
         }));
 
-        if (!empty($embeddedObjectMessageIDs)) {
+        if ($embeddedObjectMessageIDs !== []) {
             // load embedded objects
             MessageEmbeddedObjectManager::getInstance()->loadObjects('be.bastelstu.chat.message', $embeddedObjectMessageIDs);
         }

files/lib/data/message/MessageEditor.class.php
@@ -37,7 +37,7 @@ class MessageEditor extends DatabaseObjectEditor
         WCF::getDB()->beginTransaction();
 
         $result = parent::deleteAll($messageIDs);
-        if (!empty($messageIDs)) {
+        if ($messageIDs !== []) {
             AttachmentHandler::removeAttachments('be.bastelstu.chat.message', $messageIDs);
         }
 

files/lib/system/event/listener/HourlyCleanUpCronjobExecuteTemproomListener.class.php
@@ -41,7 +41,7 @@ final class HourlyCleanUpCronjobExecuteTemproomListener implements IParameterize
                 $toDelete[] = $room;
             }
         }
-        if (!empty($toDelete)) {
+        if ($toDelete !== []) {
             (new RoomAction(
                 $toDelete,
                 'delete'

files/lib/system/event/listener/RoomCanJoinBanListener.class.php
@@ -42,7 +42,7 @@ final class RoomCanJoinBanListener implements IParameterizedEventListener
             $parameters['user']->getDecoratedObject(),
             $eventObj
         );
-        if (!empty($suspensions)) {
+        if ($suspensions !== []) {
             $parameters['result'] = new PermissionDeniedException(
                 WCF::getLanguage()->getDynamicVariable('chat.suspension.info.be.bastelstu.chat.suspension.ban')
             );

commit 9dcc59f6109a764b5d409425ec8f273c124608fa

Fix UserAction::clearDeadSessions()
This got broken in 527a04db5861d86078b68683e071919b7891c1c1.

LICENSE
@@ -10,7 +10,7 @@ Additional Use Grant: You may use the Licensed Work when your application
                       uses the Licensed Work for a purpose that does neither
                       directly or indirectly generate revenue.
 
-Change Date:          2026-03-04
+Change Date:          2026-03-10
 
 Change License:       Version 2 or later of the GNU General Public License as
                       published by the Free Software Foundation.

files/lib/data/user/UserAction.class.php
@@ -6,7 +6,7 @@
  * Use of this software is governed by the Business Source License
  * included in the LICENSE file.
  *
- * Change Date: 2026-03-04
+ * Change Date: 2026-03-10
  *
  * On the date above, in accordance with the Business Source
  * License, use of this software will be governed by version 2
@@ -88,7 +88,7 @@ class UserAction extends AbstractDatabaseObjectAction
     public function clearDeadSessions()
     {
         $sessions = User::getDeadSessions();
-        if ($sessions !== []) {
+        if ($sessions === []) {
             return;
         }
         $userIDs = \array_map(static function ($item) {