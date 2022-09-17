118de57bb7cc60ceddc2faee5362df524c70bec9
Tim Düsterhus <tim@bastelstu.be>
2022-09-17 16:23:02 +0200

    Bump version

--- files/lib/system/event/listener/HourlyCleanUpCronjobExecuteChatCleanUpListener.class.php
+++ files/lib/system/event/listener/HourlyCleanUpCronjobExecuteChatCleanUpListener.class.php
@@ -6,7 +6,7 @@
  * Use of this software is governed by the Business Source License
  * included in the LICENSE file.
  *
- * Change Date: 2026-03-10
+ * Change Date: 2026-09-17
  *
  * On the date above, in accordance with the Business Source
  * License, use of this software will be governed by version 2
