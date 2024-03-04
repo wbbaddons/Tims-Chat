7b5be950e0daad1cd7e5c9a885e72680e6a85523
Tim Düsterhus <tim@bastelstu.be>
2024-03-04 20:42:52 +0100

    Bump version

--- files/lib/system/event/listener/RoomListPageTemproomListener.class.php
+++ files/lib/system/event/listener/RoomListPageTemproomListener.class.php
@@ -6,7 +6,7 @@
  * Use of this software is governed by the Business Source License
  * included in the LICENSE file.
  *
- * Change Date: 2028-01-13
+ * Change Date: 2028-03-14
  *
  * On the date above, in accordance with the Business Source
  * License, use of this software will be governed by version 3
